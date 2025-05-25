f6b997950091b75dbd546bf779f2c672a24795e4
Jinwoo-Sung <kshiva2004@outlook.com>
2025-05-25 16:54:49 +0530

    Deleted

--- perfi-main/frontend/src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <q-page>
-    <h1>This is an about page</h1>
-  </q-page>
-</template>
-
-<style>
-</style>
--- perfi-main/perfi/settings.py
+++
@@ -1,9 +0,0 @@
-def setting(db):
-    try:
-        d = {}
-        rows = db.query("SELECT * FROM setting")
-        for r in rows:
-            d[r[0]] = r[1]
-        return d
-    except:
-        return {}
